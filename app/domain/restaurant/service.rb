281d3d9c316d50a5dda134cb00c09fd2ea756d59
kamaboko <nerorubymoon@gmail.com>
2019-08-27 19:18:09 +0900

    fix service

--- app/domain/restaurant/service.rb
+++ app/domain/restaurant/service.rb
@@ -7,6 +7,19 @@ class RestaurantService
         @longitude = longitude
     end
 
+    def getLanchPlaceAll()
+        places_data = getPlacesByWalkingDistance()
+        @lanch_places = places_data["results"]
+        existNextPlaces(places_data: places_data)
+ 
+        if @lanch_places == nil then
+            return {}
+        end
+
+        return @lanch_places
+    end
+
+    private
     def getPlacesByWalkingDistance()
         return Places.getAll({
             "key" => ENV["GOOGLE_API_PLACES_KEY"],
@@ -14,6 +27,28 @@ class RestaurantService
             "radius" => 800,
             "language" => "ja",
             "opennow" => true,
+            "type" => "restaurant"
+        })
+    end
+
+    private
+    def existNextPlaces(places_data:)
+        if places_data.has_key?("next_page_token") == true then
+            add_places_data = getNextPlace(next_page_token: places_data["next_page_token"])
+            
+            add_places_data["results"].each {
+                | place | @lanch_places.push( place )
+            }
+
+            existNextPlaces(places_data: add_places_data)
+        end
+    end
+
+    private
+    def getNextPlace(next_page_token:)
+        return Places.getAll({
+            "key" => ENV["GOOGLE_API_PLACES_KEY"],
+            "next_page_token" => next_page_token
         })
     end
 end
--- spec/domain/restaurant/service_spec.rb
+++ spec/domain/restaurant/service_spec.rb
@@ -5,117 +5,70 @@ require 'rails_helper'
 RSpec.describe "800m以内で営業中の飲食店を知る" do
     context "存在する場合" do
         
-        let(:response) { <<'EOF'
-        {
-        "html_attributions": [],
-        "results": [
+        let(:first_place) {
             {
-                "geometry": {
-                    "location": {
-                        "lat": -33.8585416,
-                        "lng": 151.2100441
-                    },
-                    "viewport": {
-                        "northeast": {
-                            "lat": -33.85717672010728,
-                            "lng": 151.2112447798927
-                        },
-                        "southwest": {
-                            "lat": -33.85987637989272,
-                            "lng": 151.2085451201073
-                        }
-                    }
-                },
-                "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
-                "id": "8e980ad0c819c33cdb1cea31e72d654ca61a7065",
-                "name": "Cruise Bar",
-                "opening_hours": {
-                    "open_now": true
-                },
-                "photos": [
+                "html_attributions" => [],
+                "results" => [
                     {
-                        "height": 4032,
-                        "html_attributions": [
-                            "<a href=\"https://maps.google.com/maps/contrib/113789395029859570237/photos\">sreeju c</a>"
-                        ],
-                        "photo_reference": "CmRaAAAA2a9tcG1CYQJc8XI_FkPsQLryzMUN5QSqfw5smyJ3vUnI4FFtTk7Dx0bejwcwj22fSEJdB8UlwN9iI4B53SQR4j5B24bJ4cdacuAr26qA2siOvXbkwWAO1eovqZoY55r2EhB3WQ86RyYi0A5cUjEgPEuSGhRy7OTVtpJniKrz-pIus8-X1gQMmQ",
-                        "width": 2268
-                    }
-                ],
-                "place_id": "ChIJi6C1MxquEmsR9-c-3O48ykI",
-                "plus_code": {
-                    "compound_code": "46R6+H2 The Rocks, New South Wales",
-                    "global_code": "4RRH46R6+H2"
-                },
-                "price_level": 2,
-                "rating": 4,
-                "reference": "ChIJi6C1MxquEmsR9-c-3O48ykI",
-                "scope": "GOOGLE",
-                "types": [
-                    "bar",
-                    "restaurant",
-                    "food",
-                    "point_of_interest",
-                    "establishment"
-                ],
-                "user_ratings_total": 850,
-                "vicinity": "Level 1, 2 and 3, Overseas Passenger Terminal, Circular Quay W, The Rocks"
-            },
-            {
-                "geometry": {
-                    "location": {
-                        "lat": -33.8675829,
-                        "lng": 151.2016025
-                    },
-                    "viewport": {
-                        "northeast": {
-                            "lat": -33.86623307010728,
-                            "lng": 151.2029523298927
+                    
+                        "id" => "8e980ad0c819c33cdb1cea31e72d654ca61a7065",
+                        "name"=> "Cruise Bar",
+                        "opening_hours"=> {
+                            "open_now" => true
                         },
-                        "southwest": {
-                            "lat": -33.86893272989272,
-                            "lng": 151.2002526701073
-                        }
-                    }
-                },
-                "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
-                "id": "ce4ffe228ab7ad49bb050defe68b3d28cc879c4a",
-                "name": "Sydney Showboats",
-                "opening_hours": {
-                    "open_now": true
-                },
-                "photos": [
-                    {
-                        "height": 4912,
-                        "html_attributions": [
-                            "<a href=\"https://maps.google.com/maps/contrib/107415973755376511005/photos\">Sydney Showboats - Dinner Cruise Sydney Harbour</a>"
-                        ],
-                        "photo_reference": "CmRaAAAAxljeMDImQthQ92qixUf4e-8ptWiV0mDKu7ijTArJTxXtOkYrG3HsDfjR7XcIpfFjom50HCzhxmfJ4ajpRzGh4T_EHrk_E49ebAjQrOJtQa6vFYZjtRVxB6Y9gd_fXKEBEhCXVxGKKnXle1uUFWNNBCL0GhQ1CAKEWfYlz0OSBRSMK-l4a0kExg",
-                        "width": 7360
+                        "place_id" => "ChIJi6C1MxquEmsR9-c-3O48ykI",
+                        "types"=> [
+                            "restaurant"
+                        ]
                     }
                 ],
-                "place_id": "ChIJjRuIiTiuEmsRCHhYnrWiSok",
-                "plus_code": {
-                    "compound_code": "46J2+XJ Sydney, New South Wales",
-                    "global_code": "4RRH46J2+XJ"
-                },
-                "rating": 4.1,
-                "reference": "ChIJjRuIiTiuEmsRCHhYnrWiSok",
-                "scope": "GOOGLE",
-                "types": [
-                    "travel_agency",
-                    "restaurant",
-                    "food",
-                    "point_of_interest",
-                    "establishment"
-                ],
-                "user_ratings_total": 86,
-                "vicinity": "32 The Promenade, King Street Wharf 5, Sydney"
+                "status" => "OK",
+                "next_page_token" => "e08fd001-36ea-48cc-afbe-ac614a043cc2"
             }
-        ],
-        "status": "OK"
         }
-EOF
+        let(:next_place) { 
+            {
+            "html_attributions"=> [],
+            "results"=> [
+                {        
+                    "id": "ce4ffe228ab7ad49bb050defe68b3d28cc879c4a",
+                    "name": "Sydney Showboats",
+                    "opening_hours": {
+                        "open_now": true
+                    },
+                    "types": [
+                        "restaurant"
+                    ]
+                }
+            ],
+            "status"=> "OK",
+            "next_page_token" => "70fcb979-971d-469e-9af6-7a2f44c3a22d"
+            }
+        
+        }
+        let(:last_place) { 
+            {
+            "html_attributions"=> [],
+            "results"=> [
+                {        
+                    "id": "99904cd274e440d99a2ddc76a6fe1141",
+                    "name": "Black Cats",
+                    "opening_hours": {
+                        "open_now": true
+                    },
+                    "types": [
+                        "restaurant"
+                    ]
+                }
+            ],
+            "status"=> "OK"
+            }
+        }
+        let(:all_places) {
+            place = first_place["results"]
+            place.push(next_place["results"][0])
+            place.push(last_place["results"][0])
+            return place
         }
         let(:restaurant) {
             RestaurantService.new(latitude: "1111", longitude: "2222")
@@ -133,8 +86,20 @@ EOF
                 "radius" => 800,
                 "language" => "ja",
                 "opennow" => true,
-            }).and_return(response)
-            expect(restaurant.getPlacesByWalkingDistance).to eq response
+                "type" => "restaurant"
+            }).and_return(first_place)
+            
+            expect(Places).to receive(:getAll).with({
+                "key" => "test_key",
+                "next_page_token" => "e08fd001-36ea-48cc-afbe-ac614a043cc2"
+            }).and_return(next_place)
+
+            expect(Places).to receive(:getAll).with({
+                "key" => "test_key",
+                "next_page_token" => "70fcb979-971d-469e-9af6-7a2f44c3a22d"
+            }).and_return(last_place)
+
+            expect(restaurant.getLanchPlaceAll).to eq all_places
         end
     end
 
@@ -157,8 +122,9 @@ EOF
                 "radius" => 800,
                 "language" => "ja",
                 "opennow" => true,
+                "type" => "restaurant"
             }).and_return(response)
-            expect(restaurant.getPlacesByWalkingDistance).to eq response
+            expect(restaurant.getLanchPlaceAll).to eq response
         end
     end
   end
